5c4c9e52d08a1ad9a00fff17b73512b353bcab6f
Vihaan Singh <64571343+DarknessCODYT@users.noreply.github.com>
2020-12-26 17:19:06 +0530

    Getting Search Titles and Urls From Python Input
    Will be integrated later

---
+++ Temporary/getting_titles_urls.py
@@ -0,0 +1,30 @@
+# These are to be integrated in the project
+# Getting Links
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url_input = input("Enter Your Question: ")
+url = f"https://www.google.dz/search?q={url_input}"
+page = requests.get(f"https://www.google.dz/search?q={url_input}")
+soup = BeautifulSoup(page.content, "lxml")
+links = soup.findAll("a")
+for link in soup.find_all("a", href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
+    links = re.split(":(?=http)", link["href"].replace("/url?q=", ""))
+    print("----------------------------------------------------------------------------------------------------------------")
+    print(links)
+
+# ------------------------------------------------------------------------------------
+
+
+# Getting Titles
+
+r = requests.get(url)
+content = r.content
+soup = BeautifulSoup(content, "html.parser")
+heading = soup.find_all("h3")
+n = len(heading)
+for x in range(n):
+    titles = str.strip(heading[x].text)
+    print("----------------------------------------------------------------------------------------------------------------")
+    print(titles)
