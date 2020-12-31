b2275b758706f2894648ee27f8d97126477c30bf
Vihaan Singh <64571343+DarknessCODYT@users.noreply.github.com>
2020-12-31 19:30:18 +0530

    Links are now being filtered. Brackets removed

--- main/views.py
+++ main/views.py
@@ -1,28 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 import requests
 import re
 from bs4 import BeautifulSoup
-
 link_raw = []
+link_filter = []
 def Home(request):
     query = request.POST.get('query')
     if request.method == 'POST':
         link_raw.clear()
-        # bing = requests.get(f"https://www.bing.com/search?q={query}")
-        # google = requests.get(f"https://www.google.dz/search?q={query}")
+        link_filter.clear()
+        bing = requests.get(f"https://www.bing.com/search?q={query}")
+        google = requests.get(f"https://www.google.dz/search?q={query}")
         yahoo = requests.get(f"https://in.search.yahoo.com/search?p={query}")
-        # soup_google = BeautifulSoup(google.content, "lxml")
-        # soup_bing = BeautifulSoup(bing.content, "lxml")
+        soup_google = BeautifulSoup(google.content, "lxml")
+        soup_bing = BeautifulSoup(bing.content, "lxml")
         soup_yahoo = BeautifulSoup(yahoo.content, "lxml")
-        # for google_link in soup_google.find_all("a", href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
-        #     google_links = re.split(":(?=http)", google_link["href"].replace("/url?q=", ""))
-        #     link_raw.append(google_links)
-        # for bing_link in soup_bing.find_all("a", href=re.compile("(htt.*://.*)")):
-        #     link_raw.append(bing_link["href"])
+        for google_link in soup_google.find_all("a", href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
+            google_links = re.split(":(?=http)", google_link["href"].replace("/url?q=", ""))
+            link_raw.append(google_links)
+        for bing_link in soup_bing.find_all("a", href=re.compile("(htt.*://.*)")):
+            link_raw.append(bing_link["href"])
         for yahoo_link in soup_yahoo.find_all("a", href=re.compile("(htt.*://.*)")):
             link_raw.append(yahoo_link["href"])
-        return render(request, "results.html", {"context": link_raw})
+        for link in link_raw:
+            f1 = str(link)
+            filter1 = re.sub(r'\[\'', '', f1)
+            filter2 = re.sub(r'\'\]', '', filter1)
+            filtered_links = str(filter2)
+            link_filter.append(filtered_links)
+        return render(request, "results.html", {"context": link_filter})
 
     return render(request, "home.html")
 
@@ -30,6 +36,7 @@ def Home(request):
 
 
 def Results(request):
-    return render(request, "results.html", {"context": link_raw})
+    return render(request, "results.html", {"context": link_filter})
+
 
 
