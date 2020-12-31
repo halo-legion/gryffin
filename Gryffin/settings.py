a56cd89bfeee50434ebb1067493a1b84679c9691
Vihaan Singh <64571343+DarknessCODYT@users.noreply.github.com>
2020-12-31 23:45:24 +0530

    Update settings.py

--- Gryffin/settings.py
+++ Gryffin/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    "F:\IntelliJIdeaProjects\Gryffin\static",	
+)	
+
 django_heroku.settings(locals())
 
 
