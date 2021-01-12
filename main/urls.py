9c9404a92652efd1b45112129cebfd36e98dc6e7
Vihaan Singh <64571343+DarknessCODYT@users.noreply.github.com>
2021-01-12 19:17:03 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import Home, Results
+from .views import Home, Results, error_404_views
 urlpatterns = [
     path('', Home, name='home'),
     path('', Results, name='results'),
-
-
-
 ]
+
+handler404 = error_404_views
